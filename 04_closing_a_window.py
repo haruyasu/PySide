2176acb9b396f91a5d567fa2b7032993c631804f
Haru <harukun2002@gmail.com>
2017-08-16 21:23:02 -0700

    Closing a window

---
+++ 04_closing_a_window.py
@@ -0,0 +1,25 @@
+import sys
+from PySide import QtGui, QtCore
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        qbtn = QtGui.QPushButton('Quit', self)
+        qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(200, 200)
+
+        self.setGeometry(200, 200, 500, 500)
+        self.setWindowTitle('Quit Button')
+        self.show()
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
