784d2ca31919ae327183cf3318149188ce2994e4
Haru <harukun2002@gmail.com>
2017-08-18 22:24:10 -0700

    Signal Slots

---
+++ 15_signal_slots.py
@@ -0,0 +1,30 @@
+import sys
+from PySide import QtGui, QtCore
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        lcd = QtGui.QLCDNumber(self)
+        sld = QtGui.QSlider(QtCore.Qt.Horizontal, self)
+
+        vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(lcd)
+        vbox.addWidget(sld)
+
+        self.setLayout(vbox)
+        sld.valueChanged.connect(lcd.display)
+
+        self.setGeometry(200, 200, 500, 500)
+        self.setWindowTitle('Signal Slot')
+        self.show()
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
