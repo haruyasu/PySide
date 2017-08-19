7c90178869f6f8cb8a93318c05064be21bb67427
Haru <harukun2002@gmail.com>
2017-08-18 22:16:37 -0700

    Review Example

---
+++ 14_review_example.py
@@ -0,0 +1,42 @@
+import sys
+from PySide import QtGui
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        title = QtGui.QLabel('Title')
+        author = QtGui.QLabel('Author')
+        review = QtGui.QLabel('Review')
+
+        titleEdit = QtGui.QLineEdit()
+        authorEdit = QtGui.QLineEdit()
+        reviewEdit = QtGui.QLineEdit()
+
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(title, 1, 0)
+        grid.addWidget(titleEdit, 1, 1)
+
+        grid.addWidget(author, 2, 0)
+        grid.addWidget(authorEdit, 2, 1)
+
+        grid.addWidget(review, 3, 0)
+        grid.addWidget(reviewEdit, 3, 1, 5, 1)
+
+        self.setLayout(grid)
+
+        self.setGeometry(200, 200, 500, 500)
+        self.setWindowTitle('Review')
+        self.show()
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
