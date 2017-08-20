561f4cf6211f9661f89f529e6ce1c38e823ad89f
Haru <harukun2002@gmail.com>
2017-08-20 13:49:46 -0700

    QComboBox

---
+++ 30_qsplitter.py
@@ -0,0 +1,43 @@
+import sys
+from PySide import QtGui, QtCore
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        hbox = QtGui.QHBoxLayout(self)
+
+        topleft = QtGui.QFrame(self)
+        topleft.setFrameShape(QtGui.QFrame.StyledPanel)
+
+        toprigth = QtGui.QFrame(self)
+        toprigth.setFrameShape(QtGui.QFrame.StyledPanel)
+
+        bottom = QtGui.QFrame(self)
+        bottom.setFrameShape(QtGui.QFrame.StyledPanel)
+
+        splitter1 = QtGui.QSplitter(QtCore.Qt.Horizontal)
+        splitter1.addWidget(topleft)
+        splitter1.addWidget(toprigth)
+
+        splitter2 = QtGui.QSplitter(QtCore.Qt.Vertical)
+        splitter2.addWidget(splitter1)
+        splitter2.addWidget(bottom)
+
+        hbox.addWidget(splitter2)
+        self.setLayout(hbox)
+        QtGui.QApplication.setStyle(QtGui.QStyleFactory.create('Cleanlooks'))
+
+        self.setGeometry(200, 200, 500, 500)
+        self.setWindowTitle('Splitter')
+        self.show()
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
