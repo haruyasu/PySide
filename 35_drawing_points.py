ea8e1e561692979630c150e5a46db9cecca25308
Haru <harukun2002@gmail.com>
2017-08-20 18:59:43 -0700

    Drawing Points

---
+++ 35_drawing_points.py
@@ -0,0 +1,35 @@
+import sys, random
+from PySide import QtGui, QtCore
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(200, 200, 500, 500)
+        self.setWindowTitle('Points')
+        self.show()
+
+    def paintEvent(self, e):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        self.drawPoints(qp)
+        qp.end()
+
+    def drawPoints(self, qp):
+        qp.setPen(QtCore.Qt.red)
+        size = self.size()
+
+        for i in range(10000):
+            x = random.randint(1, size.width() - 1)
+            y = random.randint(1, size.height() - 1)
+            qp.drawPoint(x, y)
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
