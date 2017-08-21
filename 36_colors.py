e4a5bec1c81088008a6f2e1d33c02f49aad11f6a
Haru <harukun2002@gmail.com>
2017-08-20 19:44:25 -0700

    Colors

---
+++ 36_colors.py
@@ -0,0 +1,39 @@
+import sys
+from PySide import QtGui, QtCore
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(200, 200, 500, 500)
+        self.setWindowTitle('Colors')
+        self.show()
+
+    def paintEvent(self, e):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        self.drawRectangles(qp)
+
+    def drawRectangles(self, qp):
+        color = QtGui.QColor(0, 0, 0)
+        color.setNamedColor('#d4d4d4')
+        qp.setPen(color)
+
+        qp.setBrush(QtGui.QColor(200, 0, 0))
+        qp.drawRect(10, 15, 90, 60)
+
+        qp.setBrush(QtGui.QColor(255, 80, 0, 160))
+        qp.drawRect(130, 15, 90, 60)
+
+        qp.setBrush(QtGui.QColor(25, 0, 90, 200))
+        qp.drawRect(250, 15, 90, 60)
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
