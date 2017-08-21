a5e5192e40e634898362a2d9acdd755c612e068b
Haru <harukun2002@gmail.com>
2017-08-20 19:54:43 -0700

    Pen

---
+++ 37_qpen.py
@@ -0,0 +1,52 @@
+import sys
+from PySide import QtGui, QtCore
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(200, 200, 500, 500)
+        self.setWindowTitle('Pen')
+        self.show()
+
+    def paintEvent(self, e):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        self.drawLines(qp)
+        qp.end()
+
+    def drawLines(self, qp):
+        pen = QtGui.QPen(QtCore.Qt.black, 2, QtCore.Qt.SolidLine)
+        qp.setPen(pen)
+        qp.drawLine(20, 40, 250, 40)
+
+        pen.setStyle(QtCore.Qt.DashLine)
+        qp.setPen(pen)
+        qp.drawLine(20, 80, 250, 80)
+
+        pen.setStyle(QtCore.Qt.DashDotLine)
+        qp.setPen(pen)
+        qp.drawLine(20, 120, 250, 120)
+
+        pen.setStyle(QtCore.Qt.DotLine)
+        qp.setPen(pen)
+        qp.drawLine(20, 160, 250, 160)
+
+        pen.setStyle(QtCore.Qt.DashDotDotLine)
+        qp.setPen(pen)
+        qp.drawLine(20, 200, 250, 200)
+
+        pen.setStyle(QtCore.Qt.CustomDashLine)
+        pen.setDashPattern([1, 4, 5, 4])
+        qp.setPen(pen)
+        qp.drawLine(20, 240, 250, 240)
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
