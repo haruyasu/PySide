ed356fd6dcc0aa78d97f13f72c2df38f17e36b06
Haru <harukun2002@gmail.com>
2017-08-23 08:41:03 -0700

    dev

--- 40_tetris.py
+++ 40_tetris.py
@@ -1,5 +1,8 @@
-import sys
+import sys, random
 from PySide import QtGui, QtCore
+
+class Communicate(QtCore.QObject):
+    msgToSB = QtCore.Signal(str)
 
 class Tetris(QtGui.QMainWindow):
     def __init__(self):
@@ -9,6 +12,119 @@ class Tetris(QtGui.QMainWindow):
     def initUI(self):
         self.setGeometry(200, 200, 500, 500)
         self.setWindowTitle('Tetris')
+        self.Tetrisboard = Board(self)
+
+        self.setCentralWidget(self.Tetrisboard)
+
+        self.statusbar = self.statusBar()
+        self.Tetrisboard.c.msgToSB[str].connect(self.statusbar.showMessage)
+
+        self.Tetrisboard.start()
+        self.center()
+
+    def center(self):
+        screen = QtGui.QDesktopWidget().screenGeometry()
+        size = self.setGeometry()
+        self.move((screen.width() - size.width()) / 2, (screen.height() - size.height()) / 2)
+
+class Board(QtGui.QFrame):
+    BoardWidth = 10
+    BoardHeight = 22
+    Speed = 300
+
+    def __init__(self):
+        super(Board, self).__init__()
+
+        self.timer = QtCore.QBasicTimer()
+        self.isWaitingAfterLine = False
+        self.curPiece = Shape()
+
+class Tetrominoes(object):
+    NoShape = 0
+    ZShape = 1
+    SShape = 2
+    LineShape = 3
+    TShape = 4
+    SquareShape = 5
+    LShape = 6
+    MirroredLShape = 7
+
+class Shape(object):
+    coordsTable = (
+        ((0, 0), (0, 0), (0, 0), (0, 0)),
+        ((0, -1), (0, 0), (-1, 0), (-1, 1)),
+        ((0, -1), (0, 0), (1, 0), (1, 1)),
+        ((0, 1), (0, 0), (0, 1), (0, 2)),
+        ((-1, 0), (0, 0), (1, 0), (0, 1)),
+        ((0, 0), (1, 0), (0, 1), (1, 1)),
+        ((-1, -1), (0, -1), (0, 0), (0, 1)),
+        ((1, -1), (0, -1), (0, 0), (0, 1))
+    )
+
+    def __init__(self):
+        self.coords = [[0, 0 ] for i in range(4)]
+        self.pieceShape = Tetrominoes.NoShape
+
+        self.setShape(Tetrominoes.NoShape)
+
+    def shape(self):
+        return self.pieceShape
+
+    def setShape(self, shape):
+        table = Shape.coordsTable[shape]
+        for i in range(4):
+            for j in range(2):
+                self.coords[i][j] = table[i][j]
+
+        self.pieceShape = shape
+
+    def setRandomShape(self):
+        self.setShape(random.randint(1, 7))
+
+    def x(self, index):
+        return self.coords[index][0]
+
+    def y(self, index):
+        return self.coords[index][1]
+
+    def setX(self, index, x):
+        self.coords[index][0] = x
+
+    def setY(self, index, y):
+        self.coords[index][1] = y
+
+    def minX(self):
+        m = self.coords[0][0]
+        for i in range(4):
+            m = min(m, self.coords[i][0])
+        return m
+
+    def maxX(self):
+        m = self.coords[0][0]
+        for i in range(4):
+            m = max(m, self.coords[i][0])
+        return m
+
+    def minY(self):
+        m = self.coords[0][1]
+        for i in range(4):
+            m = min(m, self.coords[i][1])
+
+    def maxY(self):
+        m = self.coords[0][1]
+        for i in range(4):
+            m = max(m, self.coords[i][1])
+
+    def rotatedLeft(self):
+        if self.pieceShape == Tetrominoes.SquareShape:
+            return self
+        result = Shape()
+        result.pieceShape = self.pieceShape
+
+        for i in range(4):
+            result.setX(i, self.y(i))
+            result.setY(i, -self.x(i))
+        return result
 
 def main():
     app = QtGui.QApplication(sys.argv)
