82efbb02eb3a669d281062664d27a842c5859e62
Haru <harukun2002@gmail.com>
2017-09-03 10:54:59 -0700

    add stylesheet

--- 42_qt_designer_test.py
+++ 42_qt_designer_test.py
@@ -29,7 +29,23 @@ class Example(QtGui.QMainWindow):
         borderRadius = 'border-radius: %spx;' % (h / 2)
         buttonStyle = 'QPushButton{%s %s %s}' % (height, border, borderRadius)
         style += buttonStyle
+
+        border = 'border-width: 4px; border-color: orange;'
+        buttonHoverStyle = 'QPushButton:hover{%s}' % (border)
+        style += buttonHoverStyle
+
         ui.setStyleSheet(style)
+
+        def clickEvent(button):
+            self.ui.textField.setText(button.text())
+            style = 'QLineEdit[text="Red"]{background-color: red; color: white}'
+            style += 'QLineEdit[text="Blue"]{background-color: blue; color: white}'
+            style += 'QLineEdit[text="Yellow"]{background-color: yellow; color: black}'
+            self.ui.textField.setStyleSheet(style)
+
+        ui.redButton.clicked.connect(fnt.partial(clickEvent, ui.redButton))
+        ui.blueButton.clicked.connect(fnt.partial(clickEvent, ui.blueButton))
+        ui.yellowButton.clicked.connect(fnt.partial(clickEvent, ui.yellowButton))
 
 def main():
     app = QtGui.QApplication(sys.argv)
