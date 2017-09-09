14acc654737ae8a61f6fd72287dbd139f0108924
Haru <harukun2002@gmail.com>
2017-09-09 10:15:33 -0700

    Add

--- 43_model_view.py
+++ 43_model_view.py
@@ -42,49 +42,44 @@ class CustomListModel(QtCore.QAbstractListModel):
     def flags(self, index):
         return QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled
 
-    def setData(self, index, value, role = QtCore.Qt.EditRole):
-        if not index.isValid() or not 0 <= index.row() < len(self.__items):
-            return False
-        if role == QtCore.Qt.EditRole and value != "":
-            self.__items[index.row()]["name"] = value
-            self.dataChanged.emit(index, index)
-            return True
-        else:
-            return False
-
-    def flags(self, index):
-        return QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable
-
 class CustomListDelegate(QtGui.QStyledItemDelegate):
+
+    ITEM_SIZE_HINT = [300, 70]
+    MARGIN = 20
+    THUMB_AREA_WIDTH = 70
+    THUMB_WIDTH = 60
+    STATUS_AREA_WIDTH = 24
+    BG_DEFAULT = QtGui.QBrush(QtGui.QColor(247, 248, 242))
+    BG_SELECTED = QtGui.QBrush(QtGui.QColor(255, 255, 204))
+    BORDER_DEFAULT = QtGui.QPen(QtGui.QColor(255, 255, 255), 0.5, QtCore.Qt.SolidLine)
+    TEXT_BLACK_PEN = QtGui.QPen(QtGui.QColor(106, 107, 109), 0.5, QtCore.Qt.SolidLine)
+    SHADOW_PEN = QtGui.QPen(QtGui.QColor(220, 220, 220, 100), 1, QtCore.Qt.SolidLine)
+    FONT_H1 = QtGui.QFont("Corbel", 12, QtGui.QFont.Normal, True)
+    FONT_H2 = QtGui.QFont("Corbel", 11, QtGui.QFont.Normal, True)
+
     def __init__(self, parent=None):
         super(CustomListDelegate, self).__init__(parent)
 
     def paint(self, painter, option, index):
-        THUMB_WIDTH = 60
-        MARGIN = 5
+        selected = False
+
         if option.state & QtGui.QStyle.State_Selected:
-            bgBrush = QtGui.QBrush(QtGui.QColor(60, 60, 60))
-            bgPen = QtGui.QPen(QtGui.QColor(60, 60, 60), 0.5, QtCore.Qt.SolidLine)
-            painter.setPen(bgPen)
-            painter.setBrush(bgBrush)
-            painter.drawRect(option.rect)
+            selected = True
 
         name = index.data(QtCore.Qt.DisplayRole)
-        color = index.data(QtCore.Qt.ForegroundRole)
-        thumbName = index.data(QtCore.Qt.UserRole)
-        thumbImage = QtGui.QPixmap(os.path.join(CURRENT_PATH, "img", thumbName)).scaled(THUMB_WIDTH, THUMB_WIDTH)
-        r = QtCore.QRect(option.rect.left(), option.rect.top(), THUMB_WIDTH, THUMB_WIDTH)
-        painter.drawPixmap(r, thumbImage)
+        description = index.data(DESCRIPTION_ROLE)
+        status = index.data(STATUS_ROLE)
+        color = index.data(QtCore.Qt.BackgroundRole)
+        thumbnail = index.data(THUMB_ROLE)
 
-        pen = QtGui.QPen(color, 0.5, QtCore.Qt.SolidLine)
-        painter.setPen(pen)
-        #painter.drawText(option.rect, QtCore.Qt.AlignVCenter | QtCore.Qt.AlignLeft, "Name : " + name)
-
-        r = QtCore.QRect(option.rect.left() + THUMB_WIDTH + MARGIN, option.rect.top(), option.rect.width() - THUMB_WIDTH - MARGIN, option.rect.height())
-        painter.drawText(r, QtCore.Qt.AlignVCenter | QtCore.Qt.AlignLeft, "Name : " + name)
+        self.drawBackground(painter, option.rect, selected, color)
+        self.drawThumbnail(painter, option.rect, thumbnail)
+        self.drawName(painter, option.rect, name)
+        self.drawDescription(painter, option.rect, description)
+        self.drawStatus(painter, option.rect, status)
 
     def sizeHint(self, option, index):
-        return QtCore.QSize(100, 60)
+        return QtCore.QSize(self.ITEM_SIZE_HINT[0], self.ITEM_SIZE_HINT[1])
 
 class GUI(QtGui.QMainWindow):
     def __init__(self, parent=None):
