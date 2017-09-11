11775216be93d0e1c5611775f45602ceaf0d7d75
Haru <harukun2002@gmail.com>
2017-09-10 17:15:35 -0700

    Final

--- 43_model_view.py
+++ 43_model_view.py
@@ -2,14 +2,15 @@ import sys, os
 from PySide import QtCore, QtGui
 
 CURRENT_PATH = os.path.dirname(__file__)
-
 DESCRIPTION_ROLE = QtCore.Qt.UserRole
 STATUS_ROLE = QtCore.Qt.UserRole + 1
 THUMB_ROLE = QtCore.Qt.UserRole + 2
 
 DATA = [
-    {"name": "Dog", "description": "Animal", "status": "OK", "color": [255, 0, 0], "thumbnail": "ben.png"},
-    {"name": "Cat", "description": "Animal", "status": "OK", "color": [0, 0, 255], "thumbnail": "natalie.png"}
+    {"name":"Dog", "description":"Animal", "status":"OK", "color":[255, 0, 0], "thumbnail":"ben.png"},
+    {"name":"Cat", "description":"Animal", "status":"NOT OK", "color":[0, 0, 255], "thumbnail":"natalie.png"},
+    {"name":"Cat", "description":"Animal", "status":"NOT OK", "color":[0, 0, 255], "thumbnail":"natalie.png"},
+    {"name":"Cat", "description":"Animal", "status":"NOT OK", "color":[0, 0, 255], "thumbnail":"natalie.png"}
 ]
 
 class CustomListModel(QtCore.QAbstractListModel):
@@ -43,7 +44,6 @@ class CustomListModel(QtCore.QAbstractListModel):
         return QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled
 
 class CustomListDelegate(QtGui.QStyledItemDelegate):
-
     ITEM_SIZE_HINT = [300, 70]
     MARGIN = 20
     THUMB_AREA_WIDTH = 70
@@ -60,18 +60,60 @@ class CustomListDelegate(QtGui.QStyledItemDelegate):
     def __init__(self, parent=None):
         super(CustomListDelegate, self).__init__(parent)
 
+    def drawBackground(self, painter, rect, selected, color):
+        if selected:
+            baseColor = self.BG_SELECTED
+        else:
+            baseColor = self.BG_DEFAULT
+
+        painter.setPen(self.BORDER_DEFAULT)
+        painter.setBrush(baseColor)
+        painter.drawRoundedRect(rect, 1, 1)
+        painter.setPen(self.SHADOW_PEN)
+        painter.drawLine(rect.bottomLeft().x(), rect.bottomLeft().y() + 2, rect.bottomRight().x(), rect.bottomRight().y() + 2)
+        r = QtCore.QRect(rect.left(), rect.top(), self.THUMB_AREA_WIDTH, rect.height())
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(QtGui.QBrush(color))
+        painter.drawRoundedRect(r, 1, 1)
+
+    def drawThumbnail(self, painter, rect, thumbnail):
+        r = QtCore.QRect(rect.left() + (self.THUMB_AREA_WIDTH - self.THUMB_WIDTH) / 2, rect.top() + (self.THUMB_AREA_WIDTH - self.THUMB_WIDTH) / 2, self.THUMB_WIDTH, self.THUMB_WIDTH)
+        thumbImage = QtGui.QPixmap(os.path.join(CURRENT_PATH, "img", thumbnail)).scaled(self.THUMB_WIDTH, self.THUMB_WIDTH)
+        painter.drawPixmap(r, thumbImage)
+
+    def drawName(self, painter, rect, name):
+        painter.setFont(self.FONT_H1)
+        painter.setPen(self.TEXT_BLACK_PEN)
+        r = QtCore.QRect(rect.left() + self.THUMB_AREA_WIDTH + self.MARGIN, rect.top(), rect.width() - self.THUMB_AREA_WIDTH - self.STATUS_AREA_WIDTH - self.MARGIN * 3, rect.height() / 2)
+        painter.drawText(r, QtCore.Qt.AlignVCenter | QtCore.Qt.AlignLeft, "Name: " + name)
+        painter.setPen(self.TEXT_BLACK_PEN)
+        painter.drawLine(r.bottomLeft(), r.bottomRight())
+
+    def drawDescription(self, painter, rect, description):
+        painter.setFont(self.FONT_H2)
+        painter.setPen(self.TEXT_BLACK_PEN)
+        r = QtCore.QRect(rect.left() + self.THUMB_AREA_WIDTH + self.MARGIN, rect.top() + rect.height() / 2, rect.width() - self.THUMB_AREA_WIDTH - self.STATUS_AREA_WIDTH - self.MARGIN * 3, rect.height() / 2)
+        painter.drawText(r, QtCore.Qt.AlignVCenter | QtCore.Qt.AlignLeft, description)
+
+    def drawStatus(self, painter, rect, status):
+        painter.setFont(self.FONT_H2)
+        painter.setPen(self.TEXT_BLACK_PEN)
+        r = QtCore.QRect(rect.right() - self.STATUS_AREA_WIDTH - self.MARGIN, rect.center().y() - self.STATUS_AREA_WIDTH / 2, self.STATUS_AREA_WIDTH, self.STATUS_AREA_WIDTH)
+        statusIcon = "notok.png"
+        if status == "OK":
+            statusIcon = "ok.png"
+        statusImage = QtGui.QPixmap(os.path.join(CURRENT_PATH, "img", statusIcon))
+        painter.drawPixmap(r, statusImage)
+
     def paint(self, painter, option, index):
         selected = False
-
         if option.state & QtGui.QStyle.State_Selected:
             selected = True
-
         name = index.data(QtCore.Qt.DisplayRole)
         description = index.data(DESCRIPTION_ROLE)
         status = index.data(STATUS_ROLE)
         color = index.data(QtCore.Qt.BackgroundRole)
         thumbnail = index.data(THUMB_ROLE)
-
         self.drawBackground(painter, option.rect, selected, color)
         self.drawThumbnail(painter, option.rect, thumbnail)
         self.drawName(painter, option.rect, name)
@@ -96,14 +138,10 @@ class GUI(QtGui.QMainWindow):
         url = os.path.join(CURRENT_PATH, "img", "bg.png").replace("\\", "/")
         myListView.setStyleSheet("background: url(" + url + ") center center;")
         myListView.setVerticalScrollMode(QtGui.QAbstractItemView.ScrollPerPixel)
-
         myListModel = CustomListModel(data=DATA)
-
         myListDelegate = CustomListDelegate()
-
         myListView.setModel(myListModel)
         myListView.setItemDelegate(myListDelegate)
-
         self.setCentralWidget(myListView)
 
 def main():
