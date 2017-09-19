2c04053291e53227b6f9ad40557dd9a3e6c6ba9a
Haru <harukun2002@gmail.com>
2017-09-18 20:57:55 -0700

    Check Button

---
+++ 50_button_check.py
@@ -0,0 +1,29 @@
+import sys
+from PySide import QtCore, QtGui
+
+def print_state(state):
+    if state:
+        print("enable")
+    else:
+        print("disable")
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    main_window = QtGui.QMainWindow()
+    main_window.setWindowTitle("Check")
+    main_window.resize(300, 100)
+    panel = QtGui.QWidget()
+    checkbox = QtGui.QCheckBox("Checkbox", parent=panel)
+    button = QtGui.QPushButton("Push checkbox state", parent=panel)
+    layout = QtGui.QVBoxLayout()
+    layout.addWidget(checkbox)
+    layout.addWidget(button)
+    panel.setLayout(layout)
+    button.clicked.connect(checkbox.toggle)
+    checkbox.stateChanged.connect(print_state)
+    main_window.setCentralWidget(panel)
+    main_window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
