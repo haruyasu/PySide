16295bfe04514704a63829cd62e0c55709839cac
Haru <harukun2002@gmail.com>
2017-09-29 10:56:25 -0700

    QSequentialAnimationGroup

--- 53_button_animation.py
+++ 53_button_animation.py
@@ -4,27 +4,36 @@ from PySide import QtGui
 
 class GUI(QtGui.QWidget):
     def __init__(self):
-        QtGui.QWidget.__init__(self)
+        super(GUI, self).__init__()
+        self.initUI()
+
+    def initUI(self):
         self.setWindowTitle("Animation")
         self.resize(600, 400)
         button = QtGui.QPushButton("Animation Button", self)
         button.setGeometry(QtCore.QRect(0, 100, 100, 50))
 
+        # ----- just move
         # move_anim = self.create_move_anim(button)
-
         # move_anim.setEasingCurve(QtCore.QEasingCurve.InOutBack)
-
         # def start_move_anim():
         #     move_anim.start()
-
         # button.clicked.connect(start_move_anim)
+        # -----
 
-        par_anim = self.create_parallel_animation(button)
+        # ----- move and size prallel
+        # par_anim = self.create_parallel_animation(button)
+        # def start_par_anim():
+        #     par_anim.start()
+        # button.clicked.connect(start_par_anim)
+        # -----
 
-        def start_par_anim():
-            par_anim.start()
-
-        button.clicked.connect(start_par_anim)
+        # ----- move and size sequential
+        seq_anim = self.create_sequential_animation(button)
+        def start_seq_anim():
+            seq_anim.start()
+        button.clicked.connect(start_seq_anim)
+        # -----
 
     def create_move_anim(self, button):
         animation = QtCore.QPropertyAnimation(button, "pos")
@@ -46,10 +55,19 @@ class GUI(QtGui.QWidget):
         animation_group.addAnimation(size_change_anim)
         return animation_group
 
+    def create_sequential_animation(self, button):
+        animation_group = QtCore.QSequentialAnimationGroup()
+        move_anim = self.create_move_anim(button)
+        size_change_anim = self.create_size_change_animation(button)
+        animation_group.addAnimation(move_anim)
+        animation_group.addPause(500)
+        animation_group.addAnimation(size_change_anim)
+        return animation_group
+
 def main():
     app = QtGui.QApplication(sys.argv)
-    gui = GUI()
-    gui.show()
+    ui = GUI()
+    ui.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
