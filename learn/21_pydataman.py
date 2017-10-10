9593a45c5981a5974b41ad67b6d1f61a3d4fd4a4
haruyasu <harukun2002@gmail.com>
2017-10-09 17:26:27 -0700

    database sqlite3

--- learn/21_pydataman.py
+++ learn/21_pydataman.py
@@ -1,12 +1,38 @@
+__appname__ = "PyDataMan"
+__modlule__ = "main"
+
 from PySide.QtCore import *
 from PySide.QtGui import *
-import sys, sqlite3, re
+import sys, sqlite3, re, os, logging
 from ui import mainWindw_PyDataMan
 
+appDataPath = os.environ["APPDATA"] + "\\PyDataMan\\"
+
+if not os.path.exists(appDataPath):
+    try:
+        os.makedirs(appDataPath)
+    except Exception, e:
+        appDataPath = os.getcwd()
+
+logging.basicConfig(filename=appDataPath + "pydataman.log",
+                    format="%(asctime)-15s: %(name)-18s - %(levelname)-8s - %(module)-15s - %(funcName)-20s - %(lineno)-6d - %(message)s")
+
+logger = logging.getLogger(name="main-gui")
+
 class Main(QMainWindow, mainWindw_PyDataMan.Ui_mainWindow):
+    dbPath = appDataPath + "pydata.db"
+    dbConn = sqlite3.connect(dbPath)
+
     def __init__(self, parent=None):
         super(Main, self).__init__(parent)
         self.setupUi(self)
+
+        self.dbCursor = self.dbConn.cursor()
+        self.dbCursor.execute("""CREATE TABLE IF NOT EXISTS Main(id INTEGER PRIMARY KEY,
+                                 username TEXT, name TEXT, phone TEXT, addresss TEXT, status TEXT""")
+        self.dbConn.commit()
+
+        self.settings = QSettings(QSettings.IniFormat, QSettings.UserScope, "PyDataMan", "PyDataMan")
 
     def load_initial_settings(self):
         """Loads the initial settings for the application. Sets the mainTable colum widths."""
@@ -44,7 +70,16 @@ class Main(QMainWindow, mainWindw_PyDataMan.Ui_mainWindow):
         """Closes the application"""
         pass
 
-app = QApplication(sys.argv)
-form =Main()
-form.show()
-app.exec_()
+def main():
+    QCoreApplication.setApplicationName("PyDataMan")
+    QCoreApplication.setApplicationVersion("0.1")
+    QCoreApplication.setOrganizationName("PyDataMan")
+    QCoreApplication.setOrganizationDomain("pydataman.com")
+
+    app = QApplication(sys.argv)
+    form =Main()
+    form.show()
+    app.exec_()
+
+if __name__ == "__main__":
+    main()
