21920382591e953adfb504cd2a363a3c58ba70ba
haruyasu <harukun2002@gmail.com>
2017-10-09 21:03:00 -0700

    SQL warking

--- learn/21_pydataman.py
+++ learn/21_pydataman.py
@@ -29,26 +29,77 @@ class Main(QMainWindow, mainWindw_PyDataMan.Ui_mainWindow):
 
         self.dbCursor = self.dbConn.cursor()
         self.dbCursor.execute("""CREATE TABLE IF NOT EXISTS Main(id INTEGER PRIMARY KEY,
-                                 username TEXT, name TEXT, phone TEXT, addresss TEXT, status TEXT""")
+                                 username TEXT, name TEXT, phone TEXT, addresss TEXT, status TEXT)""")
         self.dbConn.commit()
 
         self.settings = QSettings(QSettings.IniFormat, QSettings.UserScope, "PyDataMan", "PyDataMan")
 
+        self.addData.clicked.connect(self.add_button_clicked)
+        self.removeRow.clicked.connect(self.remove_row_clicked)
+        self.load_initial_settings()
+
     def load_initial_settings(self):
         """Loads the initial settings for the application. Sets the mainTable colum widths."""
-        pass
+        self.dbCursor.execute("""SELECT * FROM Main""")
+        allRows = self.dbCursor.fetchall()
+
+        for row in allRows:
+            inx = allRows.index(row)
+            self.mainTable.insertRow(inx)
+            self.mainTable.setItem(inx, 0, QTableWidgetItem(row[1]))
+            self.mainTable.setItem(inx, 1, QTableWidgetItem(row[2]))
+            self.mainTable.setItem(inx, 2, QTableWidgetItem(row[3]))
+            self.mainTable.setItem(inx, 3, QTableWidgetItem(row[4]))
+            self.mainTable.setItem(inx, 4, QTableWidgetItem(row[5]))
 
     def add_button_clicked(self):
         """Calls the validate_fields() method, and adds the items to the table if true"""
-        pass
+        username = self.userName.text()
+        firstname = self.firstName.text()
+        phone = self.phoneNumber.text()
+        address = self.address.text()
+        approved = self.approved.isChecked()
+
+        if not self.validate_fields():
+            return False
+
+        currentRowCount = self.mainTable.rowCount()
+
+        self.mainTable.insertRow(currentRowCount)
+        self.mainTable.setItem(currentRowCount, 0, QTableWidgetItem(username))
+        self.mainTable.setItem(currentRowCount, 1, QTableWidgetItem(firstname))
+        self.mainTable.setItem(currentRowCount, 2, QTableWidgetItem(phone))
+        self.mainTable.setItem(currentRowCount, 3, QTableWidgetItem(address))
+        self.mainTable.setItem(currentRowCount, 4, QTableWidgetItem("Approved" if approved else "Not approved"))
+
+        parameters = (None, username, firstname, phone, address, str(approved))
+        self.dbCursor.execute("""INSERT INTO Main VALUES (?, ?, ?, ?, ?, ?)""", parameters)
+        self.dbConn.commit()
 
     def remove_row_clicked(self):
         """Removes the selected row from the mainTable"""
-        pass
+        currentRow = self.mainTable.currentRow()
+
+        if currentRow > -1:
+            currentUsername = (self.mainTable.item(currentRow, 0).text(), )
+            self.dbCursor.execute("""DELETE FROM Main WHERE username=?""", currentUsername)
+            self.dbConn.commit()
+            self.mainTable.removeRow(currentRow)
 
     def validate_fields(self):
         """Validated the QLineEdits based on RegEx"""
-        pass
+        self.dbCursor.execute("""SELECT username FROM Main""")
+        usernames = self.dbCursor.fetchall()
+        for username in usernames:
+            if self.userName.text() in username[0]:
+                QMessageBox.warning(self, "Warning", "Such username already exists!!!")
+                return False
+
+        if not re.match('^[2-9]\d{2}-\d{3}-\d{4}', self.phoneNumber.text()):
+            QMessageBox.warning(self, "Warning", "Phone number seems incorrect!!!")
+            return False
+
+        return True
 
     def import_action_triggered(self):
         """Database import handler"""
