eaed5470dd439250fc83ae29bf795fb98a4ced6d
haruyasu <harukun2002@gmail.com>
2017-10-17 22:56:20 -0700

    read and write

--- spread_sheet/main.py
+++ spread_sheet/main.py
@@ -11,8 +11,29 @@ wb = gc.open("test")
 sh = wb.worksheet("sheet1")
 
 # change cell_value
-sh.update_acell('B2', "update")
+# sh.update_acell('B2', "update")
 
 # get cell_list
-cell_list = sh.range('A1:D4')
-print cell_list
+# cell_list = sh.range('A1:D4')
+# print cell_list
+
+# read and write
+f = open('result.txt')
+line = f.readline()
+
+while line:
+    if line == "\n":
+        print "Empty"
+    else:
+        words = line.split()
+        worksheet = wb.worksheet(words[0])
+        values_list_retsu = worksheet.col_values(1)
+        values_list_gyo = worksheet.row_values(1)
+
+        for h in values_list_gyo:
+            for i in values_list_retsu:
+                if i == words[1]:
+                    worksheet.update_acell('E5', words[2])
+
+    line = f.readline()
+f.close
