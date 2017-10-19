9644a85b3d70bdcd0aa798d7ac58fd3fc9f14ff5
haruyasu <harukun2002@gmail.com>
2017-10-18 21:03:56 -0700

    web scraping

---
+++ web_scraping/get_nikkei_web_page.py
@@ -0,0 +1,24 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+url = "http://www.nikkei.com/markets/kabu/"
+
+html = urllib2.urlopen(url)
+soup = BeautifulSoup(html, "html.parser")
+
+span = soup.find_all("span")
+nikkei_heikin = ""
+
+for tag in span:
+    try:
+        string = tag.get("class").pop(0)
+
+        if string in "mkc-stock_prices":
+            nikkei_heikin = tag.string
+            break
+    except:
+        pass
+
+print nikkei_heikin
+
+
